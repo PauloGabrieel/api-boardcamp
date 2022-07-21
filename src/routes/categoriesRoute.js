fe5705a18846daea1cecd39170c29bd58014a6d3
PauloGabrieel <gabrieeel.c@gmail.com>
2022-07-21 17:18:55 -0300

    feat: cria endpoint POST/categories

--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -4,6 +4,7 @@ import {getCategories} from "../controllers/categoryControllers.js";
 const route = Router();
 
 route.get("/categories", getCategories);
+route.post("/categories", createCategory);
 
 
 export default route;
