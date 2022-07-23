2cc254f72684da7a7da9c848ca3ab866ba13cbcf
PauloGabrieel <gabrieeel.c@gmail.com>
2022-07-23 20:20:02 -0300

    feat: cria métodos do getGames e createGames

---
+++ src/controllers/gameControllers.js
@@ -0,0 +1,17 @@
+import connection from "../database/postgres";
+
+export async function getGames(req, res){
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+export async function creategame(req,res){
+    try {
+        const game = req.body;
+          
+    } catch (error) {
+        
+    }
+};
